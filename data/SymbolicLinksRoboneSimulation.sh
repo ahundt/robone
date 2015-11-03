b76c770dd67c899c935d3d6700f95fd01b0ad365
Andrew Hundt <ATHundt@gmail.com>
2015-11-02 22:28:41 -0500

    SymbolicLinksRoboneSimulation.sh now changes the RPATH of vrep with linuxbrew Based on: http://www.forum.coppeliarobotics.com/viewtopic.php?f=5&t=4479

--- data/SymbolicLinksRoboneSimulation.sh
+++ data/SymbolicLinksRoboneSimulation.sh
@@ -1,7 +1,7 @@
 VREPDIR=$HOME/V-REP_PRO_EDU_V3_2_2_64_Linux # Set to Vrep directory
 # VREPDIR=/Applications/V-REP_PRO_EDU_V3_2_2_Mac/vrep.app/Contents/MacOS # OSX
 
-TRACKERDIR=$HOME/Desktop/robonetracker
+TRACKERDIR=$HOME/robonetracker
 BUILDDIR=$TRACKERDIR/build
   
 LIBDIR=/lib # Change if using eclipse or OSX
@@ -12,10 +12,21 @@ LIBEXT=.so
 
 for arg in "libagast" "libv_repExtGrlInverseKinematics" "libcamodocal_brisk" "libcamodocal_calib" "libcamodocal_camera_models" "libcamodocal_camera_systems" "libcamodocal_chessboard"  "libcamodocal_fivepoint" "libcamodocal_pugixml" "libcamodocal_DBoW2" "libcamodocal_gpl" "libcamodocal_sparse_graph" "libv_repExtHandEyeCalibration" "libcamodocal_DUtilsCV" "libcamodocal_infrastr_calib" "libcamodocal_visual_odometry" "libfusionTrack64" "libv_repExtKukaLBRiiwa" "libcamodocal_DUtils" "libcamodocal_location_recognition" "libcamodocal_DVision" "libcamodocal_pose_estimation" "libceres" "libcamodocal_features2d" "libcamodocal_pose_graph" "libdevice64" "libv_repExtAtracsysFusionTrack"
 do
-ln -s $BUILDDIR/$LIBDIR/$arg$LIBEXT $VREPDIR
+    ln -s $BUILDDIR/$LIBDIR/$arg$LIBEXT $VREPDIR
 done
 
 ln -s $TRACKERDIR/modules/grl/src/lua/grl.lua $VREPDIR
 ln -s $TRACKERDIR/modules/robone/data/vrepAddOnFunc_RoboneSimulationLoadCutFile.lua $VREPDIR
 
 ln -s $TRACKERDIR/modules/robone/data/RoboneSimulation.ttt
+
+# vrep won't pick up the correct libraries if everything is built with linuxbrew
+# so change the dynamic library path loaded by vrep
+#
+# TODO: This solution isn't perfect, it may pick up versions conflicting with those
+#       used to build vrep, resulting in mysterious crashes. Try to use the libraries
+#       in the same directory as vrep whenever possible. 
+if [ -d $HOME/.linuxbrew ] ; then
+    ln -s $HOME/.linuxbrew/lib $VREPDIR
+    patchelf --set-rpath '$ORIGIN:$ORIGIN/lib' $VREPDIR/vrep
+fi
