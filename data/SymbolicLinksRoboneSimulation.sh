dd43613c660791a6e676678061a016d98be72378
Andrew Hundt <ATHundt@gmail.com>
2016-02-12 23:43:39 -0500

    new untested pivot calibration lua vrep api, hand eye and kuka driver lua APIs improved Initial implementation of pivot calibration API, depends on TRTK library. Hand Eye Calibration lua API now accepts string name parameters. Kuka iiwa lua API now takes a table of joint name strings.

--- data/SymbolicLinksRoboneSimulation.sh
+++ data/SymbolicLinksRoboneSimulation.sh
@@ -10,7 +10,7 @@ LIBDIR=/lib # Change if using eclipse or OSX
 LIBEXT=.so
 # LIBEXT=.dylib
 
-for arg in "libagast" "libv_repExtGrlInverseKinematics" "libcamodocal_brisk" "libcamodocal_calib" "libcamodocal_camera_models" "libcamodocal_camera_systems" "libcamodocal_chessboard"  "libcamodocal_fivepoint" "libcamodocal_pugixml" "libcamodocal_DBoW2" "libcamodocal_gpl" "libcamodocal_sparse_graph" "libv_repExtHandEyeCalibration" "libcamodocal_DUtilsCV" "libcamodocal_infrastr_calib" "libcamodocal_visual_odometry" "libfusionTrack64" "libv_repExtKukaLBRiiwa" "libcamodocal_DUtils" "libcamodocal_location_recognition" "libcamodocal_DVision" "libcamodocal_pose_estimation" "libceres" "libcamodocal_features2d" "libcamodocal_pose_graph" "libdevice64" "libv_repExtAtracsysFusionTrack"
+for arg in "libagast" "libv_repExtGrlInverseKinematics" "libcamodocal_brisk" "libcamodocal_calib" "libcamodocal_camera_models" "libcamodocal_camera_systems" "libcamodocal_chessboard"  "libcamodocal_fivepoint" "libcamodocal_pugixml" "libcamodocal_DBoW2" "libcamodocal_gpl" "libcamodocal_sparse_graph" "libv_repExtHandEyeCalibration" "libv_repExtPivotCalibration" "libcamodocal_DUtilsCV" "libcamodocal_infrastr_calib" "libcamodocal_visual_odometry" "libfusionTrack64" "libv_repExtKukaLBRiiwa" "libcamodocal_DUtils" "libcamodocal_location_recognition" "libcamodocal_DVision" "libcamodocal_pose_estimation" "libceres" "libcamodocal_features2d" "libcamodocal_pose_graph" "libdevice64" "libv_repExtAtracsysFusionTrack"
 do
     ln -s $BUILDDIR/$LIBDIR/$arg$LIBEXT $VREPDIR
 done
